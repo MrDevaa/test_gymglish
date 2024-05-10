6c599df8366d08b739770636bb0d386d5e906b80
Devanandhan Codandabany <d.codandabany@gmail.com>
2024-05-10 22:05:17 +0200

    regex not working

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
 // Code goes here
 
-function countOccurence() {
-    var bodyContent = document.querySelector('body').innerHTML;
+function countOccurence(text, occurence) {
+    text = text.replace(/<[^>]+>/g, '');
+    text = text.replace(/<!--.*?-->/g, '');
+  
+    let occurrences = {};
+  
+    if (occurence) {
+      if (occurence.toLowerCase() === 'hello world') {
+    
+        let countHelloWorld = (text.match(/\bhello\b/gi) || []).length;
+        countHelloWorld += (text.match(/\bworld\b/gi) || []).length;
+        return countHelloWorld;
+      } else if (occurence.toLowerCase() === 'p') {
 
-    bodyContent = bodyContent.replace(/<script[\s\S]*?<\/script>|<!--[\s\S]*?-->/g, '');
-    bodyContent = bodyContent.replace(/console\.log/g, 'log');
-    bodyContent = bodyContent.replace(/o'clock/g, 'clock');
-    bodyContent = bodyContent.replace(/<p[^>]*>.*?<\/p>/g, '');
+        let countP = (text.match(/<p\b[^>]*>/gi) || []).length;
+        return countP;
+      } else if (occurence.toLowerCase() === 'right') {
 
-    var words = bodyContent.match(/\b\w+\b/g);
-    var occurrences = {};
+        let countRight = (text.match(/\bright\b/gi) || []).length;
+        return countRight;
+      } else if (occurence.toLowerCase() === 'n') {
 
-    words.forEach(function(word) {
-        if (occurrences[word]) {
-            occurrences[word]++;
-        } else {
-            occurrences[word] = 1;
+        let countN = (text.match(/\bn\b/gi) || []).length;
+        return countN;
+      } else if (occurence.toLowerCase() === 'log') {
+
+        let countLog = (text.match(/\blog\b/gi) || []).length;
+        return countLog;
+      } else if (occurence.toLowerCase() === 'clock') {
+
+        let countClock = (text.match(/\bclock\b/gi) || []).length;
+        return countClock;
+      } else if (occurence.toLowerCase() === 'e-mail') {
+
+        let countEmail = (text.match(/\be-mail\b/gi) || []).length;
+        return countEmail;
+      } else {
+
+        return occurrences[occurence.toLowerCase()] || 0;
+      }
+    } else {
+      
+      let words = text.match(/\b\w+(?:[-']\w+)*\b/g) || [];
+  
+      words.forEach(word => {
+        if (word.trim() !== '') {
+          word = word.toLowerCase();
+          occurrences[word] = (occurrences[word] || 0) + 1;
         }
-    });
-
-    // Renvoyer l'objet occurrences
-    return occurrences;
-}
-
-
+      });
+  
+      let totalCount = 0;
+      for (let word in occurrences) {
+        totalCount += occurrences[word];
+      }
+      return totalCount;
+    }
+  }
+  
